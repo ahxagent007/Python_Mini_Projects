d1b02ca5cfde43701874220157ef2bfa5946e023
De Xian <ahx.agent007@gmail.com>
2020-01-12 02:35:41 +0600

    Flask Rest API (learning)

--- Blocking Websites (host)/website_blocker.py
+++ Blocking Websites (host)/website_blocker.py
@@ -1,7 +1,7 @@
 import time
 i = 0
 
-host_temp = 'hosts'
+host_path = r'C:\Windows\System32\drivers\etc\hosts'
 redirected = '192.168.0.1'
 website_url = ['www.facebook.com', 'www.youtube.com']
 
@@ -9,7 +9,7 @@ while True:
     print(i)
     i += 1
     time.sleep(1)
-    with open(host_temp, 'r+') as file:
+    with open(host_path, 'r+') as file:
         content = file.read()
 
         for website in website_url:
